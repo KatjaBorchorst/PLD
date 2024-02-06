564c70847c0b8d22c16e6ea6a8b48cc3ba6cfd3a
AlbertPerezB <apb99@live.com>
2024-02-06 17:09:35 +0100

    Lets rest LISP

--- LISP/RunLISP.fsx
+++ LISP/RunLISP.fsx
@@ -132,14 +132,15 @@ let rec eval s localEnv =
            printf "! %s\n" "File ended before expression was complete"
          ; infile.Close ())
       ; Nil
-  | Cons (Symbol "let", Cons (p, Cons (e1, Cons (e2, Nil)))) ->
-      let v = eval e1 localEnv
+  | Cons (Symbol "let", Cons (e1, Nil)) -> eval e1 localEnv 
+  | Cons (Symbol "let", Cons (p, Cons (e1, rest))) -> 
+      let v = eval e1 localEnv 
       (match matchPattern p v with
        | None -> raise (Lerror ("The pattern "
                                 + showSexpIndent p 12 12
                                 + " did not match the value "
                                 + showSexpIndent v 30 30))
-       | Some env -> eval e2 (env @ localEnv))
+       | Some env -> eval (Cons (Symbol "let", rest)) (env @ localEnv))
   | Cons (e1, args) -> // function application
       applyFun (eval e1 localEnv, evalList args localEnv, localEnv)
 
