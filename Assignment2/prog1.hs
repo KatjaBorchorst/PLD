da98379a6472e513f9d205963a30747cd1f3655b
AlbertPerezB <apb99@live.com>
2024-02-20 17:13:03 +0100

    prog1 lambda lift

---
+++ Assignment2/prog1.hs
@@ -0,0 +1,33 @@
+import Main (bizarre, ninka)
+-- bizarre g x = let m z = g z in
+--                 m x
+
+-- mango f u v = f v u
+
+-- dingo w =  let plip k m = w k m in
+--               let plop n = plip n 484000 in
+--                   plop 17
+
+
+-- mina = mango plys (bizarre ninka) (dingo zap)
+
+m' (g) z = (g) z
+bizarre' () g x = m' x
+
+mango f u v = f v u
+
+plip' (w) k m = (w) k m 
+
+plop' (w) n = plip' (w) n 484000
+
+-- dingo' () w = plop' () 17
+
+dingo' () w = plop' (w) 17
+
+plys x y = x + 4
+
+ninka x = 0 - x
+
+zap x y = x*y
+
+mina' = mango plys (bizarre' () ninka) (dingo' () zap)
