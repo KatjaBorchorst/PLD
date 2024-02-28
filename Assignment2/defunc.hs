f119e135e8f440b9afdaa3dc233ad4a6627c112c
AlbertPerezB <apb99@live.com>
2024-02-28 13:45:10 +0100

    defunc attempt

--- Assignment2/defunc.hs
+++ Assignment2/defunc.hs
@@ -17,3 +17,25 @@ ninka x = 0 - x
 zap x y = x*y
 
 mina' = mango plys (bizarre' () ninka) (dingo' () zap)
+
+--defunc herunder
+
+eval (M (g)) z = eval g z
+
+eval (Bizarre ()) (g, z) = eval (M (g)) x
+
+eval (Mango ()) f u v = eval f v u -- Skal argumenterene være i parentes?
+
+eval (Plip (w)) k m = eval w k m -- Igen argumenter i parentes?. I slides har de kun 1 på højre side af de implicit. Idk man
+
+eval (Plop (w)) n = eval (Plip (w)) n 484000
+
+eval (Dingo ()) w = eval (Plop (w)) 17
+
+eval (Plys ()) x y = eval x + 4
+
+eval (Ninka ()) x = 0 - eval x
+
+eval (Zap ()) x y = eval x * eval y
+
+eval (Mina ()) = eval mango () (eval (Plys () (eval Bizarre () ninka) (eval Dingo () (eval Zap ()))))
