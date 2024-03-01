42499fea3f8ee62e1c0a669eabea37b0988c0631
emiloerum <emil.oerum@hotmail.com>
2024-03-01 14:16:39 +0100

    scope.fsx fixed

--- Assignment2/scope.fsx
+++ Assignment2/scope.fsx
@@ -13,18 +13,6 @@ let g =
     f
 
 
-
-
-// let add5 = f 5 // When function is declared, x is 5. In the scope of this function, x is always 5.    
-// let add10 = f 10 // When function is declared, x is 10. In the scope of this function, x is always 10
-
-
-// printfn "Result of add 5 with 2 as argument: %i" (add5 2) // When the function was declared x was 5
-// printfn "Result of add 10 with 2 as argument: %i" (add10 2) // When the function was declared x was 10
-
-//Thus they act differently because of the static scope rules.
-    
-// b
 x <- 0
 
 let addSomething () =
@@ -32,12 +20,9 @@ let addSomething () =
     x <- x + 2
     res 
 
-// let addSomethingFun () =
-//     let res = (fun () -> x + 2)
-//     x <- x + 2
-//     res 
-
+let addSomethingInThisScope () =
+    addSomething ()
 
 printfn "addSomething first time: %i" (addSomething ())
 
-printfn "addSomething second time: %i" (addSomething ())
+printfn "addSomething second time: %i" (addSomethingInThisScope ())
